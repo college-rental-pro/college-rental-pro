a054f40d63db763e9ba0311969df58c174fdfbe0
lukemonsoon <monsonla03@uww.edu>
2020-05-05 23:08:17 -0500

    Add files via upload

---
+++ About.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+        <style>
+
+            .nav-item {
+                font-size: large;
+            }
+
+            nav {
+                /* padding-left:15% !important; */
+                padding-right: 15% !important;
+            }
+            
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg container-fluid navbar-light bg-white">
+                <a class="navbar-brand" href="Main_Page.html">
+                    <img src="logo.png" alt="College Rental Pro">
+                </a>
+            
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Landlord_Login-Signup.html">Managers and Landlords</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Student_Login-Signup.html">Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="About.html">About Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        
+
+    </body>
+
+    <script>
+
+
+
+    </script>
+</html>
---
+++ Student_Login-Signup.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+        <style>
+
+            .nav-item {
+                font-size: large;
+            }
+
+            nav {
+                /* padding-left:15% !important; */
+                padding-right: 15% !important;
+            }
+
+            #student-cards {
+                margin-top: 10%;
+            }
+
+            
+
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg container-fluid navbar-light bg-white">
+                <a class="navbar-brand" href="Main_Page.html">
+                    <img src="logo.png" alt="College Rental Pro">
+                </a>
+            
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="Landlord_Login-Signup.html">Managers and Landlords</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Student_Login-Signup.html">Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="About.html">About Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="container" id="student-cards">
+            <div class="row">
+
+                <div class="col-sm-6">
+                    <div class="card" id="student-login-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Student Login</h5>
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Email</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Email" aria-describedby="basic-addon1">
+                            </div>
+                              
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="basic-addon2">Password</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Password" aria-describedby="basic-addon2">
+                            </div>
+
+                            <button type="button" class="btn btn-outline-primary" id="student-login">Login</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="card" id="student-signup-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Student Sign Up</h5>
+                            
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">First and last name</span>
+                                </div>
+                                <input type="text" class="form-control">
+                                <input type="text" class="form-control">
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1">Email</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Email" aria-describedby="basic-addon1">
+                            </div>
+                              
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="basic-addon2">Password</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Password" aria-describedby="basic-addon2">
+                            </div>
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="basic-addon2">Confirm Password</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Confirm-Password" aria-describedby="basic-addon3">
+                            </div>
+
+                            <button type="button" class="btn btn-outline-success" id="student-signup">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </body>
+
+    <script>
+        $(document).ready(function(){
+
+            function studentLogin(){
+                $.ajax({
+                    type: "GET",
+                    url: "backend function",
+                    data: data,
+                    success: function(data){
+                        
+                    }
+                });
+            }
+
+            function studentSignup(){
+                $.ajax({
+                    type: "POST",
+                    url: "backend function",
+                    data: data,
+                    success: function(data){
+
+                    }
+                });
+            }
+
+
+            $('#student-login').click(studentLogin());
+            $('#student-signup').click(studentSignup());
+
+
+
+        });
+    </script>
+</html>
